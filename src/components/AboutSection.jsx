e68149da3bf04c2e75e043f22c92a66fecfc9f86
itzmerai <159508958+itzmerai@users.noreply.github.com>
2025-06-01 22:27:26 +0800

    resume

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -33,11 +33,11 @@ export const AboutSection = () => {
               </a>
 
                   <a
-                    href="/Ryan_AmasoraCV.pdf"
+                    href="/Ryan_Amasora_Resume.pdf"
                     download
                     className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
                   >
-                    Download CV
+                    Download Resume
                   </a>
 
             </div>
